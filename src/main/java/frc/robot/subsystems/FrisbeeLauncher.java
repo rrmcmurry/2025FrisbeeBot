44163b2b47e3a5da61358ebc37a8708e442cf896
Robby McMurry <rrmcmurry@gmail.com>
2025-10-22 16:49:00 -0400

    Update FrisbeeLauncher.java

--- src/main/java/frc/robot/subsystems/FrisbeeLauncher.java
+++ src/main/java/frc/robot/subsystems/FrisbeeLauncher.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
+import com.revrobotics.spark.SparkAbsoluteEncoder;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
@@ -15,15 +16,16 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 public class FrisbeeLauncher extends SubsystemBase {
 
     public static SparkMaxConfig DefaultConfig = new SparkMaxConfig();    
-    private SparkMax FrisbeeLaunchMotor;
-    public static final int kFrisbeeLaunchMotorCanID = 9;
+    private SparkMax FrisbeeLaunchMotor;    
     private SparkMax FrisbeeLoadingMotor;
+    public static final int kFrisbeeLaunchMotorCanID = 9;
     public static final int kFrisbeeLoadingMotorCanID = 10;
-    
+    private SparkAbsoluteEncoder flywheelencoder;
+
     static {
         DefaultConfig.smartCurrentLimit(50);
         DefaultConfig.idleMode(IdleMode.kCoast);
-        DefaultConfig.openLoopRampRate(2.0);
+        DefaultConfig.openLoopRampRate(1.0);
         DefaultConfig.inverted(true);
     }
 
@@ -31,17 +33,19 @@ public class FrisbeeLauncher extends SubsystemBase {
         FrisbeeLaunchMotor = new SparkMax(kFrisbeeLaunchMotorCanID, MotorType.kBrushless);
         FrisbeeLoadingMotor = new SparkMax(kFrisbeeLoadingMotorCanID, MotorType.kBrushless);
         FrisbeeLaunchMotor.configure(DefaultConfig, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
-        FrisbeeLoadingMotor.configure(DefaultConfig, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+        FrisbeeLoadingMotor.configure(DefaultConfig, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);        
+        flywheelencoder = FrisbeeLaunchMotor.getAbsoluteEncoder();
     }
 
     public Command fire() {
         return Commands.sequence(
-            Commands.runOnce(() -> FrisbeeLaunchMotor.set(1)),
-            Commands.waitSeconds(3.0),
-            Commands.runOnce(() -> FrisbeeLoadingMotor.set(1)),
-            Commands.waitSeconds(2.0)
+            Commands.runOnce(() -> FrisbeeLaunchMotor.set(1)), // Set Frisbee launch motor to full speed
+            Commands.waitUntil(() -> flywheelencoder.getVelocity() > 5000), // Wait until we're at 5000 RPM
+            Commands.waitSeconds(0.5), // Wait another half a second
+            Commands.runOnce(() -> FrisbeeLoadingMotor.set(1)), // Load the frisbee
+            Commands.waitSeconds(2.0) // Fire 
         ).finallyDo(() -> {
-            FrisbeeLaunchMotor.stopMotor();
+            FrisbeeLaunchMotor.stopMotor(); // let the motors coast
             FrisbeeLoadingMotor.stopMotor();
         });
     }
