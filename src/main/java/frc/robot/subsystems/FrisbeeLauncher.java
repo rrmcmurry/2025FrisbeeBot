59d052990e377b5f3ef41ed45436f9c382c32e57
Robby McMurry <rrmcmurry@gmail.com>
2025-10-30 13:36:50 -0400

    Update FrisbeeLauncher.java
    Remove unused flywheel encoder

--- src/main/java/frc/robot/subsystems/FrisbeeLauncher.java
+++ src/main/java/frc/robot/subsystems/FrisbeeLauncher.java
@@ -4,7 +4,6 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
-import com.revrobotics.spark.SparkAbsoluteEncoder;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
@@ -20,7 +19,7 @@ public class FrisbeeLauncher extends SubsystemBase {
     private SparkMax FrisbeeLoadingMotor;
     public static final int kFrisbeeLaunchMotorCanID = 10;
     public static final int kFrisbeeLoadingMotorCanID = 9;
-    private SparkAbsoluteEncoder flywheelencoder;
+    
     public boolean loaded;
 
     static {
@@ -35,7 +34,7 @@ public class FrisbeeLauncher extends SubsystemBase {
         FrisbeeLoadingMotor = new SparkMax(kFrisbeeLoadingMotorCanID, MotorType.kBrushless);
         FrisbeeLaunchMotor.configure(DefaultConfig, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
         FrisbeeLoadingMotor.configure(DefaultConfig, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);        
-        flywheelencoder = FrisbeeLaunchMotor.getAbsoluteEncoder();
+        
         loaded = true;
     }
 
@@ -47,8 +46,7 @@ public class FrisbeeLauncher extends SubsystemBase {
         if (!loaded) return Commands.none();
         
         return Commands.sequence(
-            Commands.runOnce(() -> FrisbeeLaunchMotor.set(1)), // Set Frisbee launch motor to full speed
-            // Commands.waitUntil(() -> flywheelencoder.getVelocity() > 100), // Wait until we're at 5000 RPM
+            Commands.runOnce(() -> FrisbeeLaunchMotor.set(1)), // Set Frisbee launch motor to full speed            
             Commands.waitSeconds(1.0), // Wait another half a second
             Commands.runOnce(() -> FrisbeeLoadingMotor.set(1)), // Load the frisbee
             Commands.runOnce(() -> loaded = false),
